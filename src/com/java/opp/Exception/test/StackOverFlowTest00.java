9e1fcaebdad1dfbb6f8c70efaf40795ba85ab800
Rizdev <Carvalhorizandro@gmail.com>
2025-04-17 22:45:09 +0100

    java Exception (Errors & StackOverFlow Error)

---
+++ src/com/java/opp/Exception/test/StackOverFlowTest00.java
@@ -0,0 +1,12 @@
+package com.java.opp.Exception.test;
+
+public class StackOverFlowTest00 {
+    public static void main(String[] args) {
+        recursivity();
+    }
+
+    public  static  void recursivity(){
+        recursivity();
+    }
+
+}
