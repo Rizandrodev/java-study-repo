533a07e24df51b7b3de64ffef8d37a947a9bcc3d
Rizdev <Carvalhorizandro@gmail.com>
2025-04-23 23:20:35 +0100

    java Utility classes (Wrappers)

---
+++ src/com/java/opp/Wrappers/test/Wrapperstest01.java
@@ -0,0 +1,27 @@
+package com.java.opp.Wrappers.test;
+
+public class Wrapperstest01 {
+    public static void main(String[] args) {
+        byte byteP=1;
+        short shortP=1;
+        int intP=1;
+        long longP=10L;
+        double doubleP=1;
+        char charP='W';
+        boolean booleanP=false;
+
+        Byte bytew=1;
+        Short shortw=1;
+        Integer intw=1;
+        Long longw=10L;
+        Double doublew=10D;
+        Character charw='W';
+        Boolean booleanw=false;
+
+
+        Integer intW2=Integer.parseInt("1");
+        boolean verdadeiro=Boolean.parseBoolean("true");
+        System.out.println(verdadeiro);
+        System.out.println(Character.isDigit('1'));
+    }
+}
